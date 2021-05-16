16bb69f689a2200654355706c4cbad15c8160e04
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 15:33:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(arr, callBackFunction) {
+  
+  return arr.map(callBackFunction);
+  
+}
+
+
+function reduce(arr, callBackFunction, startingPoint = 0){
+  if(startingPoint == "0") 
+    return arr.reduce(callBackFunction);
+  else 
+    return arr.reduce(callBackFunction, startingPoint);
+} 
+
+
+
