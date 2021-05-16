1200f5e427aa6aaf7a4f2e834bf4964b62d0b24f
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 15:26:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,22 +7,13 @@ const map = function (srcArr, callBackFunction) {
 }
 
 
-// function reduce(sourceArray, functions, startingPoint=0){
-//   if(startingPoint=="0") return sourceArray.reduce(functions);
-//   else return sourceArray.reduce(functions, startingPoint);
-// } 
+function reduce(sourceArray, functions, startingPoint=0){
+  if(startingPoint=="0") return sourceArray.reduce(functions);
+  else return sourceArray.reduce(functions, startingPoint);
+} 
 
-// const reduce = (srcArr, callBackFunction, startingPoint = 0) => {
+const reduce = (srcArr, callBackFunction, startingPoint = 0) => {
   
-//   return srcArr.reduce(callBackFunction, startingPoint);
+  return srcArr.reduce(callBackFunction, startingPoint);
   
-// }
-
-const reduce = function (srcArr, callBackFunction, startingPoint = 0) {
-  
-  if(startingPoint == "0") 
-    return srcArr.reduce(callBackFunction);
-  else 
-    return srcArr.reduce(callBackFunction, startingPoint);
-    
 }
